0ddcce1f33fa96e250a1cead8c79b6bed7c3ba09
clase228 <polina.mumber@gmail.com>
2023-08-30 19:37:16 +0600

    commit_2

--- src/components/protected-route/index.jsx
+++ src/components/protected-route/index.jsx
@@ -1,6 +1,6 @@
 import { Navigate,Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-export const ProtectedRoute = ({ redirectPath = "/" }) => {
+export const ProtectedRoute = ({ redirectPath = "/login" }) => {
    const isAllowed = localStorage.getItem('idInstance')
    if (!isAllowed) {
      return <Navigate to={redirectPath} replace={true} />;
